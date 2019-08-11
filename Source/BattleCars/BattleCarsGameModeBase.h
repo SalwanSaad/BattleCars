2461f485e67067a8b79dbf8ca5e724d9ba5d219f
SalwanSaad <salwansaad923@gmail.com>
2019-08-11 18:43:34 +0200

    Finished landscape

---
+++ Source/BattleCars/BattleCarsGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleCarsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLECARS_API ABattleCarsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
